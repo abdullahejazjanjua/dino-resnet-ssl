5fbc5dfcaaaf64fd84d033a1c3007ff9bc8c0951
abdullahejazjanjua <abdullahejaz822@gmail.com>
2025-08-22 04:31:00 +0500

    Removed the help from obvious args

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def args_parser():
     parser.add_argument("--num_local_crop", default=8, type=int, help="Number of local crops to use")
 
     # Model and Data specifications    
-    parser.add_argument("--dataset_path", default="tiny-imagenet-200", type=str, help="Excepts data to be in json format")
-    parser.add_argument("--model_size", default=50, type=int, help="Which model to use?")
+    parser.add_argument("--dataset_path", default="tiny-imagenet-200", type=str)
+    parser.add_argument("--model_size", default=50, type=int)
 
     # Training specifications
-    parser.add_argument("--batch_size", default=32, type=int, help="Number of images per iterations")
-    parser.add_argument("--epochs", default=30, type=int, help="Maybe this not the repository you should run :)")
-    parser.add_argument("--device", default="mps", type=str, help="Which device to train the model on?")
-    parser.add_argument("--loss_threshold", default=0.001, type=int, help="Early stopping criteria")
+    parser.add_argument("--batch_size", default=32, type=int)
+    parser.add_argument("--epochs", default=30, type=int)
+    parser.add_argument("--device", default="mps", type=str)
+    parser.add_argument("--loss_threshold", default=0.001, type=int)
 
     # Additional parametres
     parser.add_argument("--save_dir", default="logs/", type=str, help="Path to save checkpoints")
