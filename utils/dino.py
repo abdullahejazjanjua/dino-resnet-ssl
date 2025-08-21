17f39013067b29c6a74e69312e0ea9c9cb5c66cb
abdullahejazjanjua <abdullahejaz822@gmail.com>
2025-08-21 17:51:11 +0500

    Added DinoHead and DinoModel

--- utils/dino.py
+++ utils/dino.py
@@ -1,10 +1,28 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models import resnet50
+
+from dino_head import DinoHead
+
 
 class DINO(nn.Module):
-    def __init__(self):
+    def __init__(self, model_id=None, in_dim=1000):
         super().__init__()
-    
+        self.model = resnet50()
+        self.dino_head = DinoHead(in_dim=1000)
+
     def forward(self, x):
-        pass
+
+        x = self.model(x)
+        x = x.flatten(1, -1)
+        x = self.dino_head(x)
+
+        return x
+
+if __name__ == "__main__":
+    model = DINO()  
+    x = torch.randn((2, 3, 224, 224))
+    x = model(x)
+    print(f"x: {x.shape}")
+
--- utils/dino_head.py
+++ utils/dino_head.py
@@ -1,17 +1,67 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.utils.parametrizations as P
 
 class DinoHead(nn.Module):
     def __init__(
                 self, 
                 in_dim,
-                out_dim, 
-                num_layers,
-                hidden_dim,
+                K=65536,
+                bottleneck_dim=256,
+                nlayers=3, 
+                hidden_dim=2048,
                 
             ):
         super().__init__()
 
-        
+        self.input_layer = nn.Linear(in_features=in_dim, out_features=hidden_dim)
+        self.n_mlp = DinoHead._make_mlp(hidden_dim=hidden_dim, nlayers=nlayers)
+        self.out_layer = nn.Linear(in_features=hidden_dim, out_features=bottleneck_dim)
 
+        self.last_layer = P.weight_norm(
+            nn.Linear(in_features=bottleneck_dim, out_features=K, bias=False)
+        )
+
+        # Initialize weights
+        self.apply(self._init_weights)
+
+    def forward(self, x):
+
+        x = self.input_layer(x)
+
+        x = self.n_mlp(x)
+
+        x = F.normalize(x, p=2, dim=-1)
+
+        x = self.out_layer(x)
+        
+        x = self.last_layer(x)
+
+        return x
+    
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def _make_mlp(hidden_dim, nlayers):
+        layers = nn.ModuleList()
+        for _ in range(nlayers):
+            layers.extend([
+                nn.Linear(in_features=hidden_dim, out_features=hidden_dim),
+                nn.BatchNorm1d(hidden_dim),
+                nn.GELU()
+            ])
+
+        return nn.Sequential(*layers)
+    
+
+if __name__ == "__main__":
+
+    x = torch.randn((2, 3, 224, 224))
+    x = x.flatten(1, -1)
+    projection_head = DinoHead(in_dim=x.shape[-1])
+    x = projection_head(x)
+    print(f"x: {x.shape}")
