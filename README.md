d21ccbca1417ca230a31e2843ea8e489482c1207
abdullahejazjanjua <abdullahejaz822@gmail.com>
2025-08-21 19:07:21 +0500

    Removed files

--- README.md
+++ README.md
@@ -1,63 +1,14 @@
-# DINO ResNet50: Self-Supervised Feature Learning
+# TODO:
 
-This repository implements ResNet50 trained using the DINO (self-DIstillation with NO labels) self-supervised learning method. The project explores how DINO's self-supervised pretraining can enhance ResNet50's feature learning capabilities compared to traditional supervised training.
-
-## Overview
-
-DINO is a self-supervised learning approach that trains vision transformers and CNNs without labels by using knowledge distillation between different augmented views of the same image. This implementation applies DINO to ResNet50 to investigate its effectiveness on convolutional architectures.
-
-## Key Features
-
-- **Self-supervised pretraining** using DINO methodology
-- **ResNet50 backbone** for feature extraction
-
-## Model Architecture
-
-The implementation uses ResNet50 as the student and teacher networks in the DINO framework:
-- **Student network**: ResNet50 with projection head
-- **Teacher network**: Exponential moving average of student parameters
-- **Training objective**: Cross-entropy loss between student and teacher outputs
-
-## Installation
-
-```bash
-git clone https://github.com/abdullahejazjanjua/dino-resnet-ssl.git
-cd dino-resnet-ssl
-pip install -r requirements.txt
-```
-
-## Usage
-
-### Training
-> Refer to `main.py` for a list of availiable arguments
-```bash
-python main.py --epochs 30 --model_size 50 --device "cuda"  --dataset_path "path_to_your_dataset" --save_dir "path_to_save_model_checkpoints"
-```
-
-<!-- 
-## Dataset
-This model is trained on [this](https://www.kaggle.com/datasets/akash2sharma/tiny-imagenet)
- -->
-
-<!-- 
-## Experiments
-
-This repository includes experiments comparing:
-- Supervised ResNet50 baseline
-- DINO-pretrained ResNet50
-- Feature quality analysis
-- Transfer learning performance -->
-
-## Citation
-
-If you use this work, please cite the original DINO paper:
-
-```bibtex
-@inproceedings{caron2021emerging,
-  title={Emerging properties in self-supervised vision transformers},
-  author={Caron, Mathilde and Touvron, Hugo and Misra, Ishan and J{\'e}gou, Herv{\'e} and Mairal, Julien and Bojanowski, Piotr and Joulin, Armand},
-  booktitle={Proceedings of the IEEE/CVF international conference on computer vision},
-  pages={9650--9660},
-  year={2021}
-}
-```
+- [x] DinoHead
+- [x] Dino Model
+- [x] Dino Loss
+  - [x] Center update
+  - [x] linear warmup of teacher model's temperature (0.04 to 0.07 during the first 30 epochs)
+- [x] Augmentations
+- [] Dataloader
+- [] Optimizer
+  - [] Linear rampup to base lr: 0.0005 * batchsize/256
+  - [] Use cosine decay after this using cosine schedule
+- [] The weight decay also follows a cosine schedule from 0.04 to 0.4
+  - 
--- main.py
+++ main.py
@@ -2,100 +2,39 @@ import os
 import copy
 import argparse
 import logging
-from utils.misc import MetricLogger
-from utils.engine import *
-from torchvision.models import resnet50, resnet101, resnet152
-
+from utils.dino import DINO
 
 def args_parser():
     parser = argparse.ArgumentParser(description="DINO parametres")
 
-    parser.add_argument("--local_crop_scale", default=(0.05, 0.4), type=tuple)
-    parser.add_argument("--global_crop_scale", default=(0.4, 1.0), type=tuple)
-    parser.add_argument("--num_local_crop", default=8, type=int)
-    parser.add_argument("--batch_size", default=32, type=int)
-    parser.add_argument("--dataset_path", default="tiny-imagenet-200", type=str)
-    parser.add_argument("--model_size", default=50, type=int)
+    # Crops specifications
+    parser.add_argument("--local_crop_scale", default=(0.05, 0.4), type=tuple, help="Size of local crops")
+    parser.add_argument("--global_crop_scale", default=(0.4, 1.0), type=tuple, help="Size of global crops")
+    parser.add_argument("--num_local_crop", default=8, type=int, help="Number of local crops to use")
 
-    parser.add_argument("--device", default="mps", type=str)
+    # Model and Data specifications    
+    parser.add_argument("--dataset_path", default="tiny-imagenet-200", type=str, help="Excepts data to be in json format")
+    parser.add_argument("--model_size", default=50, type=int, help="Which model to use?")
 
-    parser.add_argument("--epochs", default=30, type=int)
+    # Training specifications
+    parser.add_argument("--batch_size", default=32, type=int, help="Number of images per iterations")
+    parser.add_argument("--epochs", default=30, type=int, help="Maybe this not the repository you should run :)")
+    parser.add_argument("--device", default="mps", type=str, help="Which device to train the model on?")
+    parser.add_argument("--loss_threshold", default=0.001, type=int, help="Early stopping criteria")
 
-    parser.add_argument("--save_dir", default="logs/", type=str)
-    parser.add_argument("--verbose", action="store_true")
-
-    parser.add_argument("--loss_threshold", default=0.001, type=int)
+    # Additional parametres
+    parser.add_argument("--save_dir", default="logs/", type=str, help="Path to save checkpoints")
+    parser.add_argument("--verbose", action="store_true", help="Provides more detailed information. Recommended for debugging only!")
 
     return parser
 
 
 def main(args):
     os.makedirs(args.save_dir, exist_ok=True)
-
-    logging.basicConfig(
-        level=logging.INFO,
-        filename=args.save_dir + "/log.txt",
-        filemode="a",
-        format="%(message)s",
-    )
-    logger = logging.getLogger("dino")
-    model = None
-
-    if args.model_size == 50:
-        model = resnet50(weights=None)
-    elif args.model_size == 101:
-        model = resnet101(weights=None)
-    elif args.model_size == 152:
-        model = resnet152(weights=None)
-
-    if model is None:
-        logging.critical(
-            f"Expected size to be 50, 101 and 150 but {args.model_size} provided"
-        )
-        raise Exception
-    resnet_layers = list(model.children())[:-2]
-    additional_layers = nn.Sequential(
-        nn.Flatten(),
-        nn.LazyLinear(2048),
-        nn.LayerNorm(2048),
-        nn.Linear(2048, 1024)
-    )
-    # last_layers.append(nn.Flatten())
-    # last_layers.append(nn.LazyLinear(2048))
-    # last_layers.append(nn.LayerNorm(2048))
-    # last_layers.append(nn.Linear(2048, 1024))
-
-    student_model = nn.Sequential(*resnet_layers, additional_layers)
-    student_model.register_buffer("centre", torch.ones([1, 1024]))
-
-    teacher_model = copy.deepcopy(student_model).to(args.device)
-
-    for p in teacher_model.parameters():
-        p.requires_grad = False
-    student_model.to(args.device).train()
-
-    solver = Solver(student_model, teacher_model, args)
-
-    ##############################################
-    #            training                        #
-    #############################################
-
-    solver.train()
-
-    ############################################
-    #            end training                  #
-    ############################################
-
-    # #######################################################
-    # #                    Evaluate                         #
-    # #######################################################
-
-    # solver.evaluate(student_model, teacher_model, set="val")
-
-    # ########################################################
-    # #                  End Evaluate                        #
-    # ########################################################
-
+    student_model = DINO()
+    teacher_model = copy.deepcopy(student_model)
+    
+    
 
 if __name__ == "__main__":
     args = args_parser().parse_args()
